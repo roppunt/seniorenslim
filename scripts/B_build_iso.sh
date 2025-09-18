265b4d2c5291d3b7a31a180f88f5f652c0d3be3f
roppunt <78036127+roppunt@users.noreply.github.com>
2025-09-18 10:36:18 -0700

    Update B_build_iso.sh: disable security repository

--- scripts/B_build_iso.sh
+++ scripts/B_build_iso.sh
@@ -22,7 +22,8 @@ sudo lb config \
   --archive-areas "main contrib non-free non-free-firmware" \
   --mirror-bootstrap http://deb.debian.org/debian/ \
   --mirror-binary http://deb.debian.org/debian/ \
-  --mirror-binary-security http://deb.debian.org/debian-security/
+  --mirror-binary-security http://deb.debian.org/debian-security/ \
+  --security false
 
 # Build
 sudo lb build
